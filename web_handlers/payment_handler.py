99a4ceb56a58848abc4d8932f9e1cf079f862335
dok <dokadair@gmail.com>
2020-11-01 21:29:37 +0600

    'fixes'

--- web_handlers/payment_handler.py
+++ web_handlers/payment_handler.py
@@ -10,6 +10,7 @@ payment_handler_app = web.Application()
 async def payment_handler(request: web.Request) -> str:
     print(await request.text())
     # print(await request.json())
+    print(2)
     print(request.content)
     data = json.dumps(await request.json())
     invoice = data.get('invoice')
