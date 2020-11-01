773755c791974feca2354087fa296c697e9beda5
dok <dokadair@gmail.com>
2020-11-01 22:11:40 +0600

    'fix'

--- web_handlers/payment_handler.py
+++ web_handlers/payment_handler.py
@@ -14,7 +14,7 @@ logging.basicConfig(filename='log.log', level=logging.INFO)
 async def payment_handler(request: web.Request) -> web.Response:
     data = await request.post()
     invoice = data.get('invoice')
-    return Response(body={'invoice': invoice})
+    return Response(body=json.dumps({'invoice': invoice}))
 
 
 payment_handler_app.add_routes([web.post('/', payment_handler)])
