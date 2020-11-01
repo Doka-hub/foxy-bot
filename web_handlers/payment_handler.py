372c549d28de21262d963c2071a28a3d6be68d21
dok <dokadair@gmail.com>
2020-11-01 21:34:15 +0600

    payment fix

--- web_handlers/payment_handler.py
+++ web_handlers/payment_handler.py
@@ -1,16 +1,19 @@
 from aiohttp import web
+
 import json
+
+import logging
 
 from models import objects, TGUser, Post
 
-
 payment_handler_app = web.Application()
+logging.basicConfig(filename='log.log', level=logging.INFO)
 
 
 async def payment_handler(request: web.Request) -> str:
+    logging.info('%s' % await request.text() + '%s' % request.content)
     print(await request.text())
     # print(await request.json())
-    print(2)
     print(request.content)
     data = json.dumps(await request.json())
     invoice = data.get('invoice')
