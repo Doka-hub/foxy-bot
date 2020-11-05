d6058f02f711f35f45b95f6d18db35b9317a10ae
dok <dokadair@gmail.com>
2020-11-05 21:10:37 +0600

    celery fix

--- tasks/utils/mailling.py
+++ tasks/utils/mailling.py
@@ -1,3 +1,7 @@
+from asyncio import sleep
+
+from aiogram.utils.exceptions import RetryAfter
+
 from models import objects, Channel, Article
 
 from utils.db_api.user.channel import get_channel_to_subscribe, check_user_channel_subscribed
@@ -5,10 +9,10 @@ from utils.db_api.user.channel import get_channel_to_subscribe, check_user_chann
 from loader import bot
 
 
-def make_message(language: str, preview_text: str, article_url: str) -> str:
-    ru_message = preview_text + '\n\n' + f'[Читать...]({article_url})'
-    en_message = preview_text + '\n\n' + f'[Read...]({article_url})'
-    he_message = preview_text + '\n\n' + f'[Read...]({article_url})'
+def make_message(language: str, preview_text: str, article_url: str, category: str) -> str:
+    ru_message = preview_text + f'\n\n#{category}\n\n' + f'[Читать...]({article_url})'
+    en_message = preview_text + f'\n\n#{category}\n\n' + f'[Read...]({article_url})'
+    he_message = preview_text + f'\n\n#{category}\n\n' + f'[Read...]({article_url})'
     return (
         ru_message if language == 'ru' else
         en_message if language == 'en' else
@@ -16,15 +20,16 @@ def make_message(language: str, preview_text: str, article_url: str) -> str:
     )
 
 
-async def post_to_channel(language: str, preview_text: str, article_url: str) -> None:
+async def post_to_channel(language: str, preview_text: str, article_url: str, category: str) -> None:
     channel = await objects.get(Channel, language=language)
     channel_id = channel.channel_id
-    await bot.send_message(channel_id, make_message(language, preview_text, article_url), parse_mode='markdown')
+    await bot.send_message(channel_id, make_message(language, preview_text, article_url, category),
+                           parse_mode='markdown', disable_notification=True)
     await bot.close()
 
 
-async def post_to_user(user_id: int, language: str, preview_text: str, article_url: str) -> None:
-    await bot.send_message(user_id, make_message(language, preview_text, article_url), parse_mode='markdown')
+async def post_to_user(user_id: int, language: str, preview_text: str, article_url: str, category: str) -> None:
+    await bot.send_message(user_id, make_message(language, preview_text, article_url, category), parse_mode='markdown')
     await bot.close()
 
 
@@ -38,14 +43,12 @@ async def post_news_teller(time_to_mail: str) -> None:
 
             if check_user_channel_subscribed(bot, user.user_id, channel_id):
                 if user.time_to_mail == time_to_mail:
-                    await post_to_user(user.user_id, article.category.language, f'#{article.category.name}',
-                                       article.url)
-
+                    try:
+                        await post_to_user(user.user_id, article.category.language, f'#{article.category.name}',
+                                           article.url, article.category.name)
+                    except RetryAfter:
+                        await sleep(61.0)
+                        await post_to_user(user.user_id, article.category.language, f'#{article.category.name}',
+                                           article.url, article.category.name)
     if time_to_mail == 'morning':  # очищаем статьи после утренней рассылки
         Article.truncate_table()
-
-
-async def test_():
-    print(2)
-    await bot.send_message(1092694232, 'hello')
-    await bot.close()
