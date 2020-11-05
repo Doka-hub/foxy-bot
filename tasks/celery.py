027c43e5e32779bc95bb75d17c15cf68ed0dd202
root <root@vm1465082.3ssd.had.wf>
2020-11-05 12:31:20 +0000

    tasks

--- tasks/celery.py
+++ tasks/celery.py
@@ -13,57 +13,57 @@ from data import config
 
 
 logging.basicConfig(level=logging.INFO, filename='tasks.log')
-app = Celery('tasks', broker=config.redis['host'], )
+app = Celery('celery', broker=config.redis['host'], )
 
 app.conf.timezone = config.TIMEZONE
 app.conf.beat_schedule = {
-    'test': {
-        'task': 'tasks.test',
-        'schedule': timedelta(seconds=10)
+    'test2': {
+        'task': 'test',
+        'schedule': 10.0
     },
     'parse_news': {
-        'task': 'tasks.parse_news_task',
+        'task': 'parse_news_task',
         'schedule': crontab(minute='50')
     },
     'delete_not_paid_posts': {
-        'task': 'tasks.delete_not_paid_posts_task',
+        'task': 'delete_not_paid_posts_task',
         'schedule': crontab(hour='23', minute='59')
     },
     'mailing_morning': {
-        'task': 'tasks.mailing_task',
+        'task': 'mailing_task',
         'schedule': crontab(hour='8', minute='12'),
         'args': ('morning',)
     },
     'mailing_evening': {
-        'task': 'tasks.mailing_task',
+        'task': 'mailing_task',
         'schedule': crontab(hour='20', minute='12'),
         'args': ('evening',)
     },
 }
 
 
-@app.task
+@app.task(name='test')
 def test() -> None:
     loop = asyncio.get_event_loop()
     loop.run_until_complete(test_())
     loop.close()
 
 
-@app.task
+@app.task(name='parse_news')
 def parse_news_task() -> None:
     loop = asyncio.get_event_loop()
     loop.run_until_complete(parse_news())
     loop.close()
 
 
-@app.task
+@app.task(name='mailing')
 def mailing_task(time_to_mail: str) -> None:
     loop = asyncio.get_event_loop()
     loop.run_until_complete(post_news_teller(time_to_mail))
     loop.close()
 
 
-@app.task
+@app.task(name='delete_not_paid_posts')
 def delete_not_paid_posts_task() -> None:
     loop = asyncio.get_event_loop()
     loop.run_until_complete(delete_not_paid_posts())
