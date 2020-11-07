7fecd4eebfab5848e49055ed6e294e3d52166f19
dok <dokadair@gmail.com>
2020-11-07 17:51:45 +0600

    'fix

--- tasks/utils/post.py
+++ tasks/utils/post.py
@@ -6,7 +6,7 @@ from utils.post.time_to_pay import timedelta_to_hours
 
 
 async def delete_not_paid_posts():
-    posts = await objects.execute(Post.select().where(not Post.paid))
+    posts = await objects.execute(Post.select().where(Post.paid == False))
     for post in posts:
         if timedelta_to_hours(datetime.now() - post.created) >= 72:
             await objects.delete(post)
