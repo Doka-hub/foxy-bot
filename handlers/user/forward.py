facadf136bee4bd721848417ccff9d1acfe53b6d
dok <dokadair@gmail.com>
2020-11-20 18:56:57 +0600

    fix

--- handlers/user/forward.py
+++ handlers/user/forward.py
@@ -3,3 +3,4 @@ from aiogram import types
 
 async def forward_handler(message: types.Message) -> None:
     await message.answer(str(message.forward_from_chat))
+    await message.answer(str(message.forward_from_message_id))
